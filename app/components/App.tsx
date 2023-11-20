34c4bdc36ebb2f1426ab4aeeaa3eb64ccd78a03f
KarreKult <kareemzuwa@gmail.com>
2023-11-20 16:00:30 +0100

    fixing landing page errors and loaders

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -10,30 +10,37 @@ const App = () => {
   const page = 1;
   const { isLoading, error } = useLocationsInfo(page);
 
-  if (isLoading) {
-    return <SpinnerLoader />;
-  }
-
-  if (error) {
-    return <div>Error: something went wrong while fetching data</div>;
-  }
-
   return (
     <AppLayout>
       <div className="flex-1">
         <MainSection>
-          <div className="h-96 flex-1 flex flex-col items-center justify-around">
-            <div className="flex flex-col items-center justify-between font-racing text-3xl">
-              <div>Welcome to the fantastic Universe of Rick and Morty</div>
+          {error ? (
+            <div className="h-full w-full flex items-center justify-center py-16">
+              <div>Error: something went wrong while fetching data</div>
             </div>
-            <div>Pleace <b>Click the icon</b> to Navigate to Location to explore</div>
-            <Link className="" to={`/locations/${page}`}>
-            <div >
-              <img className="w-40 h-40" src="favicon.ico" alt="icon" />
+          ) : isLoading ? (
+            <div className="h-full w-full flex items-center justify-center py-16">
+              <SpinnerLoader />
             </div>
-            </Link>
-            <p className="text-sm">"This application is the result of a coding assignment. Enjoy!"</p>
-          </div>
+          ) : (
+            <div className="h-96 flex-1 flex flex-col items-center justify-around">
+              <div className="flex flex-col items-center justify-between font-racing text-3xl">
+                <div>Welcome to the fantastic Universe of Rick and Morty</div>
+              </div>
+              <div>
+                Pleace <b>Click the yellow image</b> to Navigate to Location to
+                explore
+              </div>
+              <Link className="" to={`/locations/${page}`}>
+                <div>
+                  <img className="w-40 h-40" src="favicon.ico" alt="icon" />
+                </div>
+              </Link>
+              <p className="text-sm">
+                "This application is the result of a coding assignment. Enjoy!"
+              </p>
+            </div>
+          )}
         </MainSection>
       </div>
     </AppLayout>
